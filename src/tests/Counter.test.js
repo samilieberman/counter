2b8764cc94b160f2cc8720cf9fd75c00eb623536
BlakeOneal <blakeoneal@ufl.edu>
2022-12-02 12:44:32 -0500

    Added unit tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,42 @@
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
+
 // import necessary react testing library helpers here
 // import the Counter component here
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const welcomeMessage = screen.getByText(/Counter/i);
+  expect(welcomeMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+// we use getByTestId since the text is dynamic
+  expect(screen.getByTestId(/count/i)).toHaveTextContent('0');
 });
+
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const exp = "1";
+  userEvent.click(screen.getByRole('button', {name: '+'}));
+  // Checks to make sure the text after pressing + is 1
+  expect(screen.getByTestId(/count/i)).toHaveTextContent(exp);
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const exp = "-1";
+  userEvent.click(screen.getByRole('button', {name: '-'}));
+  // Checks to make sure the text after pressing - is -1
+  expect(screen.getByTestId(/count/i)).toHaveTextContent(exp);
 });
+
