01b88c55dba81333216395d70d264f38c3fdf606
evanbrooks0629 <evanbrooks0629@gmail.com>
2022-12-03 12:39:47 -0500

    unit testing complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,8 +12,8 @@ export default function Counter() {
     <div>
       <h2>Counter</h2>
       <p data-testid="count">{count}</p>
-      <button onClick={increment}> + </button>
-      <button onClick={decrement}> - </button>
+      <button aria-label="increment" onClick={increment}> + </button>
+      <button aria-label="decrement" onClick={decrement}> - </button>
     </div>
   );
 }
--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,43 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counterMessage = screen.getByText(/0/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const incrementButton = screen.getByRole('button', {
+    name: /increment/i
+  });
+  const decrementButton = screen.getByRole('button', {
+    name: /decrement/i
+  });
+  fireEvent.click(incrementButton);
+  const counterMessage = screen.getByText(/1/i);
+  expect(counterMessage).toBeInTheDocument();
+
+  fireEvent.click(decrementButton);
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const decrementButton = screen.getByRole('button', {
+    name: /decrement/i
+  });
+  fireEvent.click(decrementButton);
+  const counterMessage = screen.getByText(/-1/i);
+  expect(counterMessage).toBeInTheDocument();
 });
