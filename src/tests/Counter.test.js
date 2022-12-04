60a4a872486e074342d095ecb6fb477c98b9dc3d
popli123 <nylanp10@gmail.com>
2022-12-04 18:34:48 -0500

    Added 4 unit test. Now testing coverage is 100%

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,40 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, fireEvent, screen } from '@testing-library/react';
+import Counter from '../components/App';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterTitle = screen.getByText(/Counter/i);
+  expect(counterTitle).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  
+  const countValue = screen.getByTestId("count");
+  expect(countValue.textContent).toBe("0");
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const countValue = screen.getByTestId("count");
+  const increment = screen.getByRole('button', { name: '+' });
+  expect(countValue.textContent).toBe("0");
+  fireEvent.click(increment);
+  expect(countValue.textContent).toBe("1");
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const countValue = screen.getByTestId("count");
+  const decrement = screen.getByRole('button', { name: '-' });
+  expect(countValue.textContent).toBe("0");
+  fireEvent.click(decrement);
+  expect(countValue.textContent).toBe("-1");
 });
