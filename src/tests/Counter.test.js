a457c7c42d521b5f4472cac24765d01a2cec250a
Matthew Davis <matthewpalmerdavis@gmail.com>
2022-12-04 18:29:30 -0500

    Update Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,30 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
 });
 
-test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+test("renders counter message", () => {
+  const expectedVal = screen.getByText(/Counter/i);
+  expect(expectedVal).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test("should render initial count with value of 0", () => {
+  const expectedVal = screen.getByText(/0/i);
+  expect(screen.getByTestId("count")).toBe(expectedVal);
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test("clicking + increments the count", () => {
+  fireEvent.click(screen.getByText("+"));
+  var expectedVal = screen.getByText(/1/i);
+  expect(screen.getByTestId("count")).toBe(expectedVal);
+});
+
+test("clicking - decrements the count", () => {
+  fireEvent.click(screen.getByText("-"));
+  var expectedVal = screen.getByText(/-1/i);
+  expect(screen.getByTestId("count")).toBe(expectedVal);
 });
